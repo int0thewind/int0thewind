2ded364e41011a3ca3b332b46599d4b21eb2a38a
Hanzhi Yin <yinhanzhi@gmail.com>
2021-11-16 11:01:08 -0500

    fix eslint rules on function declaration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,11 @@
-/* eslint-disable react/jsx-props-no-spreading */
 import type { AppProps } from 'next/app';
 import { CssBaseline } from '@mui/material';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </>
-  );
-}
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <>
+    <CssBaseline />
+    <Component {...pageProps} />
+  </>
+);
 
 export default MyApp;
