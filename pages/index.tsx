cfce53f11f3eeeb89a8a9e9421942193021b4ed1
Hanzhi Yin <yinhanzhi@gmail.com>
2021-11-17 16:37:27 -0500

    icon page finished

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
+import { forwardRef } from 'react';
 import type { NextPage, GetStaticProps, InferGetStaticPropsType } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
 import Divider from '@mui/material/Divider';
-import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { Me, fetchMe } from '../src/personal';
+
+const GitHubIcon = forwardRef(() => <FontAwesomeIcon icon={faGithub} size="2x" />);
+const LinkedInIcon = forwardRef(() => <FontAwesomeIcon icon={faLinkedin} size="2x" />);
 
 type HomePageProps = { me: Me };
 
@@ -16,24 +20,24 @@ export const getStaticProps: GetStaticProps<HomePageProps> = async () => {
 };
 
 const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({ me }) => (
-  <Grid container>
+  <>
     <Head>
       <title>{`${me.legalName.pinyin} | About Me`}</title>
     </Head>
-    <Grid item>
-      <Typography variant="body1" sx={{ whiteSpace: 'pre-line' }}>
-        {me.bio.work.replaceAll('\n', '\n\n')}
-      </Typography>
-    </Grid>
-    <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center', '*': { m: 1 } }}>
+    <Typography variant="body1" sx={{ whiteSpace: 'pre-line', p: 2 }}>
+      {me.bio.work.replaceAll('\n', '\n\n')}
+    </Typography>
+    <Divider />
+    <Box component="footer" sx={{ display: 'flex', justifyContent: 'center', '*': { m: 1, cursor: 'pointer' } }}>
       <Link href={me.socialMedia.github} passHref>
-        <FontAwesomeIcon icon={faGithub} size="2x" />
+        <GitHubIcon />
       </Link>
       <Link href={me.socialMedia.linkedin} passHref>
-        <FontAwesomeIcon icon={faLinkedin} size="2x" />
+        <LinkedInIcon />
       </Link>
-    </Grid>
-  </Grid>
+    </Box>
+    <Divider />
+  </>
 );
 
 export default Home;
