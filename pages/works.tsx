bdcf6e64cade6c48b71f73dc0cd4c4c415e30a26
Hanzhi Yin <yinhanzhi@gmail.com>
2021-11-17 19:47:24 -0500

    get static props for works page

--- pages/works.tsx
+++ pages/works.tsx
@@ -1,10 +1,30 @@
-import type { NextPage } from 'next';
+import type { GetStaticProps, InferGetStaticPropsType, NextPage } from 'next';
+import Head from 'next/head';
 import Typography from '@mui/material/Typography';
+import {
+  Me, Projects, fetchMe, fetchProjects,
+} from '../src/personal';
 
-const Works: NextPage = () => (
-  <Typography>
-    Contents will be placed soon!
-  </Typography>
+type WorksPageProps = {
+  me: Me,
+  projects: Projects
+};
+
+export const getStaticProps: GetStaticProps<WorksPageProps> = async () => {
+  const me = await fetchMe();
+  const projects = await fetchProjects();
+  return { props: { me, projects } };
+};
+
+const Works: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({ me, projects }) => (
+  <>
+    <Head>
+      <title>{`${me.legalName.pinyin} | Works`}</title>
+    </Head>
+    <Typography>
+      Contents will be placed soon!
+    </Typography>
+  </>
 );
 
 export default Works;
