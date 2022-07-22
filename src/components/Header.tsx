f0d55513ab0e4505ecdc16bd877079bc3ef0b3ad
Hanzhi Yin <yinhanzhi@gmail.com>
2022-07-22 10:29:33 -0400

    make header

---
+++ src/components/Header.tsx
@@ -0,0 +1,31 @@
+import Divider from './ui-components/Divider';
+import { cvLink } from '../personal';
+import Link from 'next/link';
+
+type HeaderItems = {
+  [key: string]: { link: string };
+};
+
+const headerItems: HeaderItems = {
+  'About Me': { link: '/' },
+  Works: { link: '/works' },
+  CV: { link: cvLink },
+};
+
+const Header = () => (
+  <header>
+    <Divider />
+    <div className="flex flex-nowrap flex-row justify-evenly">
+      {Object.entries(headerItems).map(([displayName, { link }]) => (
+        <Link key={displayName} href={link} passHref>
+          <h6 className="hover:underline hover:cursor-pointer">
+            {displayName}
+          </h6>
+        </Link>
+      ))}
+    </div>
+    <Divider />
+  </header>
+);
+
+export default Header;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import type { AppProps } from 'next/app';
+import Header from '../components/Header';
+import Container from '../components/ui-components/Container';
 import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <Container>
+    <Header />
+    <Component {...pageProps} />
+  </Container>
 );
 
 export default MyApp;
