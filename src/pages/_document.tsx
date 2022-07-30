11c8c449ec1427f220033656b806d24da070c12c
Hanzhi Yin <yinhanzhi@gmail.com>
2022-07-30 12:19:14 -0400

    add normalize.css

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,10 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
           rel="stylesheet"
         />
+        <link
+          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
